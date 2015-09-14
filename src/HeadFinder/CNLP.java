9cedd3793491b897b32555d276c6c7c3170c2c5e
spimicha <spimicha@gmail.com>
2015-09-14 12:37:37 +0200

    Directory Structure Change

---
+++ src/HeadFinder/CNLP.java
@@ -0,0 +1,357 @@
+package HeadFinder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.trees.HeadFinder;
+import edu.stanford.nlp.trees.SemanticHeadFinder;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
+import edu.stanford.nlp.util.CoreMap;
+
+public class CNLP {
+	
+	
+	public static void main(String[] args) throws IOException{
+
+		String fileLocation = new String("C:\\Users\\spimi\\workspace\\Thesis\\Data\\QuestionClassification\\train_5500.label.txt");//
+		new CNLP(fileLocation);
+		
+	}
+	
+	static Tree nPhrase = null;
+	
+	public CNLP(String file) throws IOException{
+		
+
+		
+		// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+		// NER, parsing, and coreference resolution
+		List<String> trainingData = loadData(file);
+		List<String[]> featureList = new ArrayList<String[]>();
+		// "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		Properties props = new Properties();
+		props.setProperty("ssplit.isOneSentence", "true");
+		props.setProperty("annotators", "tokenize, ssplit, parse");
+		
+		
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		SemanticHeadFinder headFinder = new SemanticHeadFinder();
+		List<String> questionList;
+		List<String> posList;
+		for(int i=0; i<trainingData.size(); i++){
+			nPhrase = null;
+			questionList = new ArrayList<String>();
+			posList = new ArrayList<String>();
+			// read some text in the text variable
+			String questionText = trainingData.get(i);
+			//questionText.replaceAll("'", "`");
+			//String questionText = "What is the proper name of a female walrus ?";
+			//String questionText = "What is the sales tax in Minessoata ?";
+			//String questionText = "What year did the Titanic sink ?";
+			//String questionText = "What is an atom ?";
+			// create an empty Annotation just with the given text
+			//String questionText = "What is a group of turkeys called ?";
+			Annotation document = new Annotation(questionText);
+			// run all Annotators on this text
+			pipeline.annotate(document);
+			// these are all the sentences in this document // a CoreMap is essentially a Map that uses class objects as keys and // has values with custom types
+			CoreMap sentence = document.get(SentencesAnnotation.class).get(0);
+			
+			// this is the parse tree of the current sentence
+			Tree questionParseTree = sentence.get(TreeAnnotation.class);
+			//head finder object	 
+			for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+				 // this is adding the text of the token to questionList
+		        questionList.add(token.get(TextAnnotation.class));
+		        // this is adding the POS tag of the token to the posList
+		        posList.add(token.get(PartOfSpeechAnnotation.class));
+			}
+			String whWord = getWhWord(questionList.get(0));
+			String[] wordAndLabel = extractHeadWord(questionList, posList, questionText, questionParseTree, headFinder);
+			//System.out.println(i+" "+whWord + " " + wordAndLabel[0] + " " + wordAndLabel[1]);
+			String[] arr = {whWord, wordAndLabel[0], wordAndLabel[1]};
+			featureList.add(arr);
+		}
+		writeData(featureList);
+	}
+		
+	
+
+	/**
+	 * Algorithm to extract head word. Uses method QTypeRegex 
+	 * @param question
+	 * @param headWord
+	 * @param label
+	 * @param sentence
+	 * @return String of extracted question head
+	 */
+	public static String[] extractHeadWord(List<String> questionList, List<String> posList, String question, Tree tree, SemanticHeadFinder headFinder) {
+		if(questionList.get(0).equals("When") || questionList.get(0).equals("Where") || questionList.get(0).equals("Why")){
+			String[] returnArray = {null, null};
+			return returnArray;
+		}
+		if(questionList.get(0).equals("How")){
+			String[] returnArray = {questionList.get(1), posList.get(1)};
+			return returnArray;
+		}
+		if(questionList.get(0).equals("What")){
+			for(String condition:checkRegex(question)){
+				if(!condition.equals("HUM:desc")){
+					String[] returnArray = {condition, null};
+					return returnArray;
+				}
+			}
+		}
+		if(questionList.get(0).equals("Who") && checkRegex(question).equals("HUM:desc")){
+			String[] returnArray = {"HUM:desc", null};
+			return returnArray;
+		}
+		int candidateIndex = getCandidate(tree, headFinder, questionList);
+		String candidateWord = "";
+		String candidateLabel = "";
+		if(candidateIndex > -1){
+			candidateWord = questionList.get(candidateIndex);
+			candidateLabel = posList.get(candidateIndex);
+		}
+		if(candidateLabel.startsWith("NN")){
+			String[] returnArray = {candidateWord, candidateLabel};
+			return returnArray;
+		}
+		for(int i=0; i<posList.size()-1; i++){		
+			 if(posList.get(i).startsWith("NN")){
+				 String[] returnArray = {questionList.get(i), posList.get(i)};
+				 return returnArray;
+			 }
+		}
+		String[] returnArray = {null, null};
+		return returnArray;
+	}
+	
+	
+	/**
+	 * Takes a String question checks against all the regular expression patterns.
+	 * If pattern is found, condition name is added to List
+	 * @param question
+	 * @return ArrayList of condition names (Strings)
+	 */
+	public static List<String> checkRegex(String question){
+		String condition = "";
+		List<String> returnList = new ArrayList<String>();
+		String descDef1 = "W|what (is|are)( (a|an|the))?( \\w+)( \\w+)?.?.?";
+		String descDef2 = "W|what (do|does) .+ mean.?.?";
+		String entySubs = "W|what is|are .+ (composed of|made out of).?.?";
+		String descDesc = "W|what does .+ do.?.?";
+		String entyTerm = "W|what do you call .+";
+		String descReas1 = "W|what (causes|cause).*";
+		String descReas2 = "W|what (is|are) .+ used for.?.?";
+		String abbrExp = "W|what (does|do) .+ stand for.?.?";
+		String humDesc = "W|who (is|was) [A-Z]\\w+.?.?";
+		String[] conditionName = { "DESC:def", "DESC:def", "ENTY:sub",
+				"DESC:desc", "ENTY:term", "DESC:reason", "DESC:reason",
+				"ABBR:exp", "HUM:desc" };
+		String[] conditions = { descDef1, descDef2, entySubs, descDesc,
+				entyTerm, descReas1, descReas2, abbrExp, humDesc };
+
+		for (int i = 0; i < conditions.length; i++) {
+			Pattern pattern = Pattern.compile(conditions[i]);
+			Matcher m = pattern.matcher(question);
+			if (m.matches()) {
+				condition = conditionName[i];
+				returnList.add(condition);
+			}
+		}
+		return returnList;
+	}
+
+	
+	/**
+	 * Takes as input Coremap sentence, and returns and array of type
+	 * string with the headString and then the label
+	 * @param sentence
+	 * @return
+	 */
+	public static int getCandidate (Tree tree, SemanticHeadFinder headFinder, List<String> questionList){
+		Tree backupTree = tree.deepCopy();
+		Tree head = getSemanticHead(tree.firstChild(), tree , headFinder);
+		String wordIndexString = head.getChild(0).label().toString();
+		int headWordIndex = Integer.parseInt(wordIndexString.substring(wordIndexString.lastIndexOf("-")+1, wordIndexString.length()))-1;
+		
+		String candidateWord = questionList.get(headWordIndex);
+		if(candidateWord.equals("name" )|| candidateWord.equals("type") || candidateWord.equals("kind") || candidateWord.equals("genre") || candidateWord.equals("group")){
+			headWordIndex = addPostFix (backupTree, headFinder)-1;
+		}
+		return headWordIndex;
+	}
+		
+	
+	/**
+	 * Get semantic head of current tree. Bottom up procedure.
+	 * @param node
+	 * @param parent
+	 * @param headFinder
+	 * @return semantic head or null
+	 */
+	public static Tree getSemanticHead(Tree node, Tree parent, SemanticHeadFinder headFinder) {
+		int index = -1000;
+		for (Tree child : node.children()) {
+			if (parent.value().equals("NP") && nPhrase == null) {//Set NP in case of no NN being found
+				nPhrase = node;
+			}
+			getSemanticHead(child, node, headFinder);
+		}
+		if (!node.isLeaf() && !node.isPreTerminal()) {
+			Tree head = headFinder.determineHead(node);
+			index = parent.objectIndexOf(node);	
+			parent.removeChild(index);
+			parent.addChild(index, head);
+		}
+		if (parent.label().toString().equals("ROOT")) {// parent.label().toString()=="ROOT"
+			return parent.getChild(index);
+		}
+		else{
+			return null;
+		}
+	}
+	
+	
+	/**
+
+	/**
+	 * If any head word of type name, type, kind, genre, group, PostFix fixes
+	 * the head word to the other noun-phrase in the question. Uses BFS traversal
+	 * @param node
+	 * @param parent
+	 * @param phraseList
+	 * @param headFinder
+	 * @param headWord
+	 * @return List of Pre-terminals (NN) suitable
+	 */
+	public static int addPostFix(Tree rootNode, SemanticHeadFinder headFinder) {
+		int wordIndex = -1;
+		List<Tree> visited = new ArrayList<Tree>();
+	    Queue<Tree> queue = new LinkedList<Tree>();
+	    queue.add(rootNode);
+	    visited.add(rootNode);
+	    while(!queue.isEmpty()){
+	    	Tree node = (Tree) queue.remove();
+	    	for(Tree child:node.children()){
+	    		if(!visited.contains(child)){
+	    			visited.add(child);
+	    			if(child.value().equals("PP")){
+	    				List<Tree> grandChildren = child.getChildrenAsList();
+	    				if(grandChildren.size()>1 && grandChildren.get(0).value().equals("IN") && grandChildren.get(1).value().equals("NP")){
+	    					Tree childCopy = child.deepCopy();
+	    					childCopy.setValue("ROOT");
+	    					Tree head = getSemanticHead(childCopy.getChildrenAsList().get(1), childCopy, headFinder);
+	    					String wordIndexString = head.getChild(0).label().toString();
+	    					wordIndex = Integer.parseInt(wordIndexString.substring(wordIndexString.lastIndexOf("-")+1, wordIndexString.length()));
+	    				}
+	    			}
+	    			queue.add(child);
+	    		}
+	    	}
+	    }    
+		return wordIndex;
+	}
+
+
+	
+	/**
+	 * PostFix helper
+	 * @param node
+	 * @param headWord
+	 * @return false if one of leaves is equal to the head word
+	 */
+	public static boolean CheckLeaves(Tree node, String headWord) {
+		List<Tree> leaves = node.getLeaves();
+		for (Tree leaf : leaves) {
+			if (leaf.toString().equals(headWord)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Feature 1: wh-word
+	 * @param question
+	 * @return string wh-word or rest
+	 */
+	public String getWhWord(String questionWord){
+		if(questionWord.equals("What"))
+			return "what";
+		if(questionWord.equals("Which"))
+			return "which";
+		if(questionWord.equals("When"))
+			return "when";
+		if(questionWord.equals("Where"))
+			return "where";
+		if(questionWord.equals("Who"))
+			return "who";
+		if(questionWord.equals("How"))
+			return "how";
+		if(questionWord.equals("Why"))
+			return "why";
+		else
+			return "rest";
+	}
+	
+	
+	
+	/**
+	 * Load training Data
+	 * @param fileLocation
+	 * @return List of strings with category description names
+	 * @throws IOException
+	 */
+	public List<String> loadData(String fileLocation) throws IOException{
+		List<String> lines = new ArrayList<String>();
+		File file = new File(fileLocation);
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line = br.readLine();
+		while (line != null){
+			String[] newLine = line.split("\\s", 2);
+		    lines.add(newLine[1]);
+		    line = br.readLine();
+		}
+		return lines;
+	}
+	
+	
+	public void writeData(List<String[]> featureList) throws FileNotFoundException, UnsupportedEncodingException{
+		PrintWriter writer = new PrintWriter("C:\\Users\\spimi\\workspace\\Thesis\\src\\features.txt", "UTF-8");
+		for(int i=0; i<featureList.size(); i++){	
+			writer.println(featureList.get(i)[0]+"\t"+featureList.get(i)[1]+"\t"+featureList.get(i)[2]);	
+		}
+		writer.close();
+	}
+
+	public static String[] toList(String question){
+		return question.split("\\s");
+	}
+
+}
---
+++ src/HeadFinder/main.py
@@ -0,0 +1,222 @@
+import nltk
+from nltk.corpus import wordnet as wn
+import re, sys, os
+from nltk.stem import WordNetLemmatizer
+
+
+def GetWordShape(theString):
+    """Return whether the first letter of each word begins with capital,
+    lower, mixed, digits or other
+    """
+    isUpperCase = False
+    isLowerCase = False
+    isDigit = False
+    theString = GetUnigram(theString)
+    for i in theString:
+        if i[0].isalnum():
+            if i[0].isalpha:
+                if i[0].isupper():
+                    isUpperCase = True
+                if i[0].islower():
+                    isLowerCase = True
+            if i[0].isnumeric():
+                isDigit = True
+    if isUpperCase==True and isLowerCase==True and isDigit==False:
+        return 'mixed'
+    if isUpperCase==True and isLowerCase==False and isDigit==False:
+        return 'upper_case'
+    if isUpperCase==False and isLowerCase==True and isDigit==False:
+        return 'lower_case'
+    if isUpperCase==False and isLowerCase==False and isDigit==True:
+        return 'all_digits'
+    else:
+        return 'other'
+        
+
+def PreProcess(theString):
+    """Returns string in form of list, where each item is either a word or punctuation"""
+    theString = re.sub(r"([\w/'+$\s-]+|[^\w/'+$\s-]+)\s*", r"\1 ", theString).strip()
+    return re.split("\s", theString)
+
+
+def GetUnigram(theString):
+    """Returns list of Unigrams"""
+    return theString.split(' ')
+
+
+def GetBigram(theString):
+    """Returns list of Bigrams"""
+    unigrams = GetUnigram(theString)
+    returnList = []
+    for i in range(len(unigrams)-1):
+        returnList.append(unigrams[i]+' '+unigrams[i+1])
+    return returnList
+
+
+def GetTrigram(theString):
+    """Returns list of Trigrams"""
+    unigrams = GetUnigram(theString)
+    returnList = []
+    for i in range(len(unigrams)-2):
+        returnList.append(unigrams[i]+' '+unigrams[i+1]+' '+unigrams[i+2])
+    return returnList
+
+
+def getCategories(fileLocation):
+    """Returns the categories (coarse & fine grained)"""
+    subCatList = []
+    catList = []
+    with open(fileLocation) as f:
+        data = f.read().split('\n')
+    for i in data:
+        theTuple = i.split('\t')
+        theTuple[0] = theTuple[0].strip()
+        if theTuple[0]!='ABBR' and theTuple[0]!='ENTY' and theTuple[0]!='DESC' and theTuple[0]!='HUM' and theTuple[0]!='LOC' and theTuple[0]!='NUM':
+            subCatList.append(theTuple)
+        else:
+            catList.append(theTuple)
+    return catList, subCatList
+
+
+def MostSimilarCategory(optimumSense, categories):
+    """Returns most similar category based on sense given
+    using path similarity"""
+    categoryScores = []
+    theSynsets = []
+    for i in range(len(categories)):
+        score = 0.0
+        theSynset = None
+        for j in categories[i][1].split(' '):
+            currentScore = 0.0
+            currentCatSynset = wn.synsets(j)
+            if currentCatSynset:
+                for k in currentCatSynset:
+                    currentScore = optimumSense.path_similarity(k)
+                    if currentScore > score:
+                        score = currentScore
+                        theSynset = k
+                        #print score, theSynset, optimumSense
+        theSynsets.append(theSynset)
+        categoryScores.append(score)
+    maxVal = max(categoryScores)
+    returnList = []
+    for i in range(len(categoryScores)):
+        if categoryScores[i] == maxVal:
+            returnList.append(categories[i])
+    return returnList
+
+
+
+def FinalDecision(MostSimilarList, optimumSense):
+    """If more than one category with (equal) max path similarity score, this returns a final 
+    decision by taking into summing all path similarities words in description (where one exists) 
+    and dividing by the number of words used"""
+    categoryScores = []
+    theSynsets = []
+    for i in range(len(MostSimilarList)):
+        score = 0.0
+        theSynset = None
+        numWords = 0
+        for j in MostSimilarList[i][1].split(' '):#for each word in definition
+            currentScore = 0.0
+            currentCatSynset = wn.synsets(j)
+            if currentCatSynset:
+                numWords += 1
+                kScore = 0.0
+                for k in currentCatSynset:#for each synset of current word def
+                    kScore = optimumSense.path_similarity(k)
+                    if kScore > currentScore:
+                        currentScore = kScore
+            score += currentScore
+        score = score / numWords
+    return MostSimilarList[i], score, numWords
+                    
+                
+
+
+def Algo2(question, headWord, label):
+    """computes the maximum number of common words between gloss of this sense and gloss of any sense 
+    of the context words. Among all head word senses, the sense which results in the maximum common 
+    words is chosen as the optimal sense and is returned"""
+    synsets = wn.synsets(headWord)
+    h = [synsets[i].definition() for i in range(len(synsets))]
+    q = question
+    count, index = 0, 0
+    maxCount = -1
+    optimum = None
+    for s in h:#for each sense s for h do
+        count = 0
+        for w in q.split(' '):#for each context word w in q do  
+            y = wn.synsets(w)
+            wSenses = [y[i].definition() for i in range(len(y))]#get senses of w
+            sumMax = 0
+            for wSense in wSenses:#for each sense        
+                for word in wSense:#and each word of each sense
+                    if word in s.split(' '):#if common word found
+                        sumMax += 1
+            count = count + sumMax
+        if count > maxCount:
+            maxCount = count
+            optimum = s
+            index = h.index(s)
+    return synsets[index]
+
+
+def getHypernym(depth, sense):
+    """Returns the hypernym string of the sense at the required depth"""
+    theName = [wn.synset(sense.name())]
+    for i in range(depth):
+        newName = wn.synset(theName[0].name()).hypernyms()
+        if newName:
+            theName = newName
+            continue
+        else:
+            break
+    return theName[0]
+
+
+def getFeatureLists(featuresLoc):
+    """Loads file and returns list of features already computed """
+    with open(featuresLoc) as f:
+        lines = f.read().strip()
+        lines = lines.split('\n')
+    features = []
+    for line in lines:
+        features.append(line.split('\t'))
+    return features
+
+
+def loadQuestions(file):
+    """Loads and returns list of questions"""
+    returnList = []
+    with open(file) as f:
+        lines = f.read().strip()
+        lines = lines.split('\n')
+    for line in lines:
+        returnList.append(re.split("\w+:\w+", line, maxsplit=1)[1])
+    return returnList
+
+
+
+def main(questionLoc, categoriesLoc, featuresLoc):
+    categories, subCategories = [],[]
+    categories, subCategories = getCategories(categoriesLoc)
+    questions = loadQuestions(questionLoc) 
+    features = getFeatureLists(featuresLoc)
+
+
+    for i in range(len(questions)):
+        headWord = features[i][1]
+        label = features[i][2]
+        print i, headWord, label
+        if ':' not in headWord:
+            optimalSense = Algo2(questions[i], headWord, label)
+            disambedSens = getHypernym(5, optimalSense)
+        
+
+questionLoc = "C:\\Users\\spimi\\workspace\\Thesis\\Data\\QuestionClassification\\train_5500.label.txt"   
+categoriesLoc = "C:\\Users\\spimi\\workspace\\Thesis\\Data\\QuestionClassification\\categories.txt"
+featuresLoc = "C:\\Users\\spimi\\workspace\\Thesis\\src\\features.txt"
+
+
+main(questionLoc, categoriesLoc, featuresLoc)
